004dd5e1d6b75f7dba2327fe22ad888b8d3b95c8
RuslanPostnikov <rp02081992@gmail.com>
2022-03-16 00:22:50 +0200

    hw12

--- hw_10-13/src/App.js
+++ hw_10-13/src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import Component from "./Components/hw10/Component";
 import TodoBox from "./Components/hw11/TodoBox";
+import Card from "./Components/hw12/Card";
 
 
 function App() {
   return (
     <div className="App">
       {/*<Component />*/}
-        <TodoBox />
+      {/*  <TodoBox />*/}
+        <Card>
+            <Card.Body>
+                <Card.Title>Title</Card.Title>
+                <Card.Text>Text</Card.Text>
+            </Card.Body>
+        </Card>
     </div>
   );
 }
---
+++ hw_10-13/src/Components/hw12/Card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Body from "./Body";
+import Title from "./Title";
+import Text from "./Text";
+
+class Card extends React.Component {
+    static Body = Body;
+    static Title = Title;
+    static Text = Text;
+
+    render() {
+        return <div className="card">{this.props.children}</div>;
+    }
+}
+
+export default Card;
+
